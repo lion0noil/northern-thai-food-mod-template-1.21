3336298bda1fff502176ea8ea671b58305448be3
lion0noil <poopetlion@gmail.com>
2025-02-17 21:56:33 +0700

    Initial commit

---
+++ src/main/java/net/lion/northernthaifoodmod/NorthernThaiFoodModClient.java
@@ -0,0 +1,10 @@
+package net.lion.northernthaifoodmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NorthernThaiFoodModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
