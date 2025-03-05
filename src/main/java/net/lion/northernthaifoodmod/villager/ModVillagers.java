7470dbba3fb63fbbd91cec3b6f2dc574d27dd48d
lion0noil <poopetlion@gmail.com>
2025-03-05 16:58:08 +0700

    Update

---
+++ src/main/java/net/lion/northernthaifoodmod/villager/ModVillagers.java
@@ -0,0 +1,43 @@
+package net.lion.northernthaifoodmod.villager;
+
+import com.google.common.collect.ImmutableSet;
+import net.fabricmc.fabric.api.object.builder.v1.world.poi.PointOfInterestHelper;
+import net.lion.northernthaifoodmod.NorthernThaiFoodMod;
+import net.lion.northernthaifoodmod.block.ModBlocks;
+import net.minecraft.block.Block;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.Identifier;
+import net.minecraft.village.VillagerProfession;
+import net.minecraft.world.poi.PointOfInterestType;
+
+public class ModVillagers {
+    public static final RegistryKey<PointOfInterestType> SELL_POI_KEY = registerPoiKey("sell_poi");
+    public static final PointOfInterestType SELL_POI = registerPOI("sell_poi",ModBlocks.SELL_TRADES);
+
+    public static final VillagerProfession SELL = registerProfession("sell_trades_employee", SELL_POI_KEY);
+
+
+    private static VillagerProfession registerProfession(String name, RegistryKey<PointOfInterestType> type) {
+        return Registry.register(Registries.VILLAGER_PROFESSION, Identifier.of(NorthernThaiFoodMod.MOD_ID, name),
+                new VillagerProfession(name, entry -> entry.matchesKey(type), entry -> entry.matchesKey(type),
+                        ImmutableSet.of(), ImmutableSet.of(), SoundEvents.ENTITY_VILLAGER_WORK_LIBRARIAN));
+    }
+
+    private static PointOfInterestType registerPOI(String name, Block block) {
+        return PointOfInterestHelper.register(Identifier.of(NorthernThaiFoodMod.MOD_ID, name),
+                1, 1, block);
+    }
+
+    private static RegistryKey<PointOfInterestType> registerPoiKey(String name) {
+        return RegistryKey.of(RegistryKeys.POINT_OF_INTEREST_TYPE, Identifier.of(NorthernThaiFoodMod.MOD_ID, name));
+    }
+
+
+    public static void registerVillagers() {
+        NorthernThaiFoodMod.LOGGER.info("Registering Villagers for " + NorthernThaiFoodMod.MOD_ID);
+    }
+}
