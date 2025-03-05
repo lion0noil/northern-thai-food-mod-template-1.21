af3b45be4b996bd6469c31017fa7536583f04c43
lion0noil <poopetlion@gmail.com>
2025-03-05 17:39:55 +0700

    Update

--- src/main/java/net/lion/northernthaifoodmod/NorthernThaiFoodMod.java
+++ src/main/java/net/lion/northernthaifoodmod/NorthernThaiFoodMod.java
@@ -2,6 +2,7 @@ package net.lion.northernthaifoodmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.object.builder.v1.trade.TradeOfferHelper;
 import net.fabricmc.fabric.api.registry.CompostingChanceRegistry;
 import net.fabricmc.fabric.api.registry.FuelRegistry;
 import net.lion.northernthaifoodmod.block.ModBlocks;
@@ -9,6 +10,11 @@ import net.lion.northernthaifoodmod.item.ModItemGroups;
 import net.lion.northernthaifoodmod.item.ModItems;
 import net.lion.northernthaifoodmod.villager.ModVillagers;
 import net.lion.northernthaifoodmod.world.gen.ModWorldGeneration;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.village.TradeOffer;
+import net.minecraft.village.TradedItem;
+import net.minecraft.village.VillagerProfession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -40,5 +46,15 @@ public class NorthernThaiFoodMod implements ModInitializer {
 
 		CompostingChanceRegistry.INSTANCE.add(ModItems.CHILI, 0.5f);
 		CompostingChanceRegistry.INSTANCE.add(ModItems.CHILI_SEED, 0.25f);
+
+		TradeOfferHelper.registerVillagerOffers(ModVillagers.SELL,1,factories -> {
+			factories.add((entity, random) -> new TradeOffer(
+					new TradedItem(ModItems.RICE,10),
+					new ItemStack(Items.EMERALD,1),4,7,0.04f));
+
+			factories.add((entity, random) -> new TradeOffer(
+					new TradedItem(ModItems.BEAN,8),
+					new ItemStack(Items.EMERALD,1),4,7,0.04f));
+		});
 	}
 }
